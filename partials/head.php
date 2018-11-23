170ea39ab77dbdbffc80250f39734029bce3a537
YGeyskens <yves.geyskens@student.hepl.be>
2018-11-23 14:13:37 +0100

    added template partials

---
+++ partials/head.php
@@ -0,0 +1,24 @@
+<title>ChocoWeb</title>
+<!--
+
+Eatery Cafe Template
+
+http://www.templatemo.com/tm-515-eatery
+
+-->
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="Fabriquant de chocolat officiel pour les étudiants du groupe 2285 à la haute école de la province de Liège">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+<!-- MAIN CSS -->
+<link rel="stylesheet" href="css/templatemo-style.css">
---
+++ partials/home.php
@@ -0,0 +1,53 @@
+<!-- HOME -->
+<section id="home" class="slider" data-stellar-background-ratio="0.5">
+     <div class="row">
+
+               <div class="owl-carousel owl-theme">
+                    <div class="item item-first">
+                         <figure class="item-bg">
+                              <img src="./content/slide1.jpg" alt="Tasse de chcocolat chaud" class="item-img">
+                         </figure>
+                         <div class="caption">
+                              <div class="container">
+                                   <div class="col-md-8 col-sm-12">
+                                        <h3><?php echo('Coucou, je vend du chocolat !'); ?></h3>
+                                        <h1>Our mission is to provide an unforgettable experience</h1>
+                                        <a href="#team" class="section-btn btn btn-default smoothScroll">Meet our chef</a>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="item item-second">
+                         <figure class="item-bg">
+                              <img src="./content/slide2.jpg" alt="Des pralines au chocolat" class="item-img">
+                         </figure>
+                         <div class="caption">
+                              <div class="container">
+                                   <div class="col-md-8 col-sm-12">
+                                        <h3>Your Perfect Breakfast</h3>
+                                        <h1>The best dinning quality can be here too!</h1>
+                                        <a href="#menu" class="section-btn btn btn-default smoothScroll">Discover menu</a>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="item item-third">
+                         <figure class="item-bg">
+                              <img src="./content/slide3.jpg" alt="Des petits cupcakes au chocolat avec des pépites" class="item-img">
+                         </figure>
+                         <div class="caption">
+                              <div class="container">
+                                   <div class="col-md-8 col-sm-12">
+                                        <h3>New Restaurant in Town</h3>
+                                        <h1>Enjoy our special menus every Sunday and Friday</h1>
+                                        <a href="#contact" class="section-btn btn btn-default smoothScroll">Reservation</a>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+
+     </div>
+</section>
