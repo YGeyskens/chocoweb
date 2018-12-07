f5548e30429fa592936ed0e8566e632f8e74daab
YGeyskens <yves.geyskens@student.hepl.be>
2018-12-07 11:11:57 +0100

    added model.php and modified chef & slide.php

--- models/Chef.php
+++ models/Chef.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once('models/model.php');
 class Chef
 {
   public $id;
@@ -19,24 +19,8 @@ class Chef
     $this->job = $result['job'];
   }
   public function queryChef($id, $locale){
-    $host = '127.0.0.1';
-    $db   = 'sgc_choco';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
 
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    $options = [
-      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
-    try {
-      $pdo = new PDO($dsn, $user, $pass, $options);
-    } catch (\PDOException $e) {
-      throw new \PDOException($e->getMessage(), (int)$e->getCode());
-    }
-
+    $pdo = getConnectionToDb();
     $query = $pdo->prepare('SELECT c.id, c.img, c.name, ct.description, ct.job, cs.network, cs.url
                           FROM chefs c
                           JOIN chefs_translations ct ON ct.chef_id = c.id AND ct.locale = ?
--- models/Slide.php
+++ models/Slide.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once('models/model.php');
 class Slide
 {
   public $id;
@@ -22,24 +22,7 @@ class Slide
     $this->button = $result['btn_label'];
   }
   public function querySlide($id, $locale){
-    $host = '127.0.0.1';
-    $db   = 'sgc_choco';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
-
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    $options = [
-      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
-    try {
-      $pdo = new PDO($dsn, $user, $pass, $options);
-    } catch (\PDOException $e) {
-      throw new \PDOException($e->getMessage(), (int)$e->getCode());
-    }
-
+    $pdo = getConnectionToDb();
     $query = $pdo->prepare('SELECT s.id, s.img_src, st.img_alt, st.pre, st.title, st.btn_href, st.btn_label
                           FROM slides s
                           JOIN slide_translations st ON st.slides_id = s.id AND st.locale = ?
