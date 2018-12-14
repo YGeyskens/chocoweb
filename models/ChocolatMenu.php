59351c7a552a4fd434f3ac2dc5725a969cb01389
YGeyskens <yves.geyskens@student.hepl.be>
2018-12-14 15:43:36 +0100

    added menus content injection

--- models/ChocolatMenu.php
+++ models/ChocolatMenu.php
@@ -2,42 +2,51 @@
 include_once('models/database.php');
 class ChocolatMenu
 {
-  public $id;
-  public $src;
-  public $alt;
-  public $price;
-  public $name;
-  public $ingredients;
-  function __construct($id = null, $locale = null)
-  {
-    $result = $this->queryMenu($id,$locale);
-    $this->id = $result->id;
-    $this->src = $result->img_src;
-    $this->alt = $result->img_alt;
-    $this->name = $result->name;
-    $this->price = $result->price;
-    $this->ingredients = $this->queryIngredient($id,$locale);
-  }
-  public function queryMenu($id, $locale){
+    public $id;
+    public $src;
+    public $alt;
+    public $price;
+    public $name;
+    public $ingredients;
+    function __construct($id = null, $locale = null)
+    {
+        $result = $this->queryMenu($id,$locale);
+        $this->id = $result->id;
+        $this->src = $result->img_src;
+        $this->alt = $result->img_alt;
+        $this->name = $result->name;
+        $this->price = $result->price;
+        $this->ingredients = $this->queryIngredient($id,$locale);
+    }
+    public function queryMenu($id, $locale){
 
-    $pdo = getConnectionToDb();
-    $query = $pdo->prepare('SELECT ch.id, ch.img_src, ch.price, cht.img_alt, cht.name
+        $pdo = getConnectionToDb();
+        $query = $pdo->prepare('SELECT ch.id, ch.img_src, ch.price, cht.img_alt, cht.name
                           FROM nos_chocolats ch
                           JOIN nos_chocolats_translations cht ON cht.chocolat_id = ch.id AND cht.locale = :locale
                           WHERE ch.id = :id ;');
-    $arr = [":id"=>$id, ":locale"=>$locale];
-    $query->execute($arr);
-    return $query->fetch();
-  }
-  public function queryIngredient($id,$locale){
-    $pdo = getConnectionToDb();
-    $query = $pdo->prepare('SELECT i.id, it.name
+        $arr = [":id"=>$id, ":locale"=>$locale];
+        $query->execute($arr);
+        return $query->fetch();
+    }
+    public function queryIngredient($id,$locale){
+        $pdo = getConnectionToDb();
+        $query = $pdo->prepare('SELECT i.id, it.name
                           FROM ingredients i
                           JOIN ingredients_translations it ON it.ingredient_id = i.id AND it.locale = :locale
                           JOIN ingredient_chocolat im on  im.ingredient_id = i.id
                           WHERE im.chocolat_id = :id ;');
-    $arr = [":id"=>$id, ":locale"=>$locale];
-    $query->execute($arr);
-    return $query->fetchAll();
-  }
+        $arr = [":id"=>$id, ":locale"=>$locale];
+        $query->execute($arr);
+        return $query->fetchAll();
+    }
+
+    public function getIngredients(){
+        return implode(' / ', array_map(function($ingredient){
+            return $ingredient->name;
+        }, $this->ingredients));
+    }
+    public function getPrice(){
+        return number_format($this->price / 100, 2, ',', '&nbsp;') . '&nbsp;€';
+    }
 }
--- views/menu.php
+++ views/menu.php
@@ -1,40 +1,42 @@
 <!-- MENU-->
 <?php
-  require('controllers/Menu.php');
-  $menu = new Menu;
+require('controllers/Menu.php');
+$menu = new Menu;
 ?>
 <section id="menu" data-stellar-background-ratio="0.5">
-     <div class="container">
-          <div class="row">
+    <div class="container">
+        <div class="row">
 
-               <div class="col-md-12 col-sm-12">
-                    <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
-                         <h2>Our Menus</h2>
-                         <h4>Tea Time &amp; Dining</h4>
-                    </div>
-               </div>
-               <?php foreach ($menu->menus as $menu) : ?>
-               <div class="col-md-4 col-sm-6">
+            <div class="col-md-12 col-sm-12">
+                <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
+                    <h2>Our Menus</h2>
+                    <h4>Tea Time &amp; Dining</h4>
+                </div>
+            </div>
+            <?php foreach ($menu->menus as $menu) : ?>
+                <div class="col-md-4 col-sm-6">
                     <!-- MENU THUMB -->
                     <div class="menu-thumb">
-                         <a href="images/menu-image1.jpg" class="image-popup" title="American Breakfast">
-                              <img src="<?=$menu->src; ?>" class="img-responsive" alt="<?=$menu->alt; ?>">
+                        <a href="images/menu-image1.jpg" class="image-popup" title="American Breakfast">
+                            <img src="<?=$menu->src; ?>" class="img-responsive" alt="<?=$menu->alt; ?>">
 
-                              <div class="menu-info">
-                                   <div class="menu-item">
-                                        <h3><?=$menu->name; ?></h3>
-                                        <?php foreach($menu->ingredients as $ingredient): ?>
-                                          <p><?=$ingredient->name; ?></p>
-                                        <?php endforeach;?>
-                                   </div>
-                                   <div class="menu-price">
-                                        <span><?=$menu->price; ?></span>
-                                   </div>
-                              </div>
-                         </a>
+                            <div class="menu-info">
+                                <div class="menu-item">
+                                    <h3><?=$menu->name; ?></h3>
+
+                                    <p>
+                                        <?= $menu->getIngredients(); ?>
+                                    </p>
+
+                                </div>
+                                <div class="menu-price">
+                                    <span><?=$menu->getPrice(); ?></span>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-               </div>
-               <?php endforeach;?>
-          </div>
-     </div>
+                </div>
+            <?php endforeach;?>
+        </div>
+    </div>
 </section>
